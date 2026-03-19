30273da2b82afb18087c776e1a7be9422f54d511
Vinay Rajput <vinaypawar692002@gmail.com>
2026-03-19 22:19:16 +0530

    changes

--- frontend/src/api/searchApi.js
+++ frontend/src/api/searchApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/searches"
+  baseURL: "https://louder-assignment-50dq.onrender.com/api/searches"
 });
 
 export const createSearch = (query) => API.post("/", { query });
